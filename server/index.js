c7ee30a459ac3b7c83602ebbce0a3f27977e75e3
Paul Phillips <paulphillips217@gmail.com>
2020-04-25 15:54:04 -0400

    added the rest of the backend

--- server/index.js
+++ server/index.js
@@ -1,24 +1,139 @@
+if (process.env.NODE_ENV !== "production") {
+  const result = require("dotenv").config({ path: "./server/variables.env" });
+  if (result.error) {
+    throw result.error;
+  }
+  console.error(result.parsed);
+}
+
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const Datastore = require("nedb");
+const authorizeSpotify = require("./authorizeSpotify");
+const accessToken = require("./accessToken");
+const spotifyData = require("./spotifyData");
+
+const clientUrl = process.env.CLIENT_URL;
+const app = express();
+const db = new Datastore();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const isDev = process.env.NODE_ENV !== "production";
 const PORT = process.env.PORT || 5000;
 
-const app = express();
-
 // Priority serve any static files.
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+if (isDev) {
+  app.use(express.static(path.resolve(__dirname, "../client/public")));
+} else {
+  app.use(express.static(path.resolve(__dirname, "../client/build")));
+}
 
 // test endpoint to see if the server is running
 app.get("/ping", (req, res) => {
   res.send("It is ALIVE!!!");
 });
 
-// Answer API requests.
+// Answer API requests.  this isn't used anymore, so eventually delete it
 app.get("/api", function (req, res) {
-  console.log("got a hit");
   res.set("Content-Type", "application/json");
   res.send('{"message":"Hello from the custom server!"}');
+});
+
+// test endpoint to get the environment setting
+app.get("/node-env", (req, res) => {
+  res.send(`NODE_ENV: ${process.env.NODE_ENV}<br/>
+            PORT: ${process.env.PORT}<br/>
+            CLIENT_URL: ${process.env.CLIENT_URL}<br/>
+            `);
+});
+
+app.get("/login", authorizeSpotify);
+
+app.get("/callback", accessToken.getSpotifyAccessToken, (req, res, next) => {
+//  console.log("callback - credentials: " + JSON.stringify(req.credentials));
+  db.insert(req.credentials, (err) => {
+    if (err) {
+      next(err);
+    } else {
+      res.redirect(`${clientUrl}/?authorized=true`);
+    }
+  });
+});
+
+app.get("/history", (req, res) => {
+  accessToken.getAccessToken(db, (accessToken) => {
+    const url = "https://api.spotify.com/v1/me/player/recently-played?limit=10";
+
+    spotifyData
+      .getSpotifyData(accessToken, url)
+      .then((data) => {
+        const arr = data.items.map((e) => ({
+          played_at: e.played_at,
+          track_name: e.track.name,
+        }));
+        res.json(arr);
+      })
+      .catch((err) => console.log(err));
+  });
+});
+
+app.get("/playlists/:offset/:limit", (req, res) => {
+  accessToken.getAccessToken(db, (accessToken) => {
+    const url = `https://api.spotify.com/v1/me/playlists?offset=${req.params.offset}&limit=${req.params.limit}`;
+
+    spotifyData
+      .getSpotifyData(accessToken, url)
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => console.log(err));
+  });
+});
+
+app.get("/playlist/:id", (req, res) => {
+  accessToken.getAccessToken(db, (accessToken) => {
+    const url = `https://api.spotify.com/v1/playlists/${req.params.id}`;
+
+    spotifyData
+      .getSpotifyData(accessToken, url)
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => console.log(err));
+  });
+});
+
+app.get("/playlist-tracks/:id/:offset/:limit", (req, res) => {
+  accessToken.getAccessToken(db, (accessToken) => {
+    const url = `https://api.spotify.com/v1/playlists/${req.params.id}/tracks?offset=${req.params.offset}&limit=${req.params.limit}`;
+
+    spotifyData
+      .getSpotifyData(accessToken, url)
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => console.log(err));
+  });
+});
+
+app.post("/queue-track/:uri", (req, res) => {
+  accessToken.getAccessToken(db, (accessToken) => {
+    console.log("queue track uri: " + req.params.uri);
+    const url = `https://api.spotify.com/v1/me/player/queue?uri=${req.params.uri}`;
+
+    spotifyData
+      .postSpotifyData(accessToken, url)
+      .then((data) => {
+        console.log("queue track data: " + data);
+        res.json(data);
+      })
+      .catch((err) => console.log(err));
+  });
 });
 
 // All remaining requests return the React app, so it can handle routing.
