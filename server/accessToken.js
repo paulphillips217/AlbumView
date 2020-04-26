6805505c32342cc29dbce562be28570eca1e0483
Paul Phillips <paulphillips217@gmail.com>
2020-04-26 10:38:40 -0400

    added postgresql

--- server/accessToken.js
+++ server/accessToken.js
@@ -1,14 +1,14 @@
-const fetch = require('node-fetch');
-const spotify = require('./credentials');
+const fetch = require("node-fetch");
+const spotify = require("./credentials");
 
 const getSpotifyAccessToken = (req, res, next) => {
   const { code } = req.query;
 
   if (code) {
-    const url = 'https://accounts.spotify.com/api/token';
+    const url = "https://accounts.spotify.com/api/token";
 
     const data = {
-      grant_type: 'authorization_code',
+      grant_type: "authorization_code",
       code,
       redirect_uri: spotify.redirect_uri,
       client_id: spotify.client_id,
@@ -16,23 +16,23 @@ const getSpotifyAccessToken = (req, res, next) => {
     };
 
     const headers = {
-      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
     };
 
     const searchParams = new URLSearchParams();
 
-    Object.keys(data).forEach(prop => {
+    Object.keys(data).forEach((prop) => {
       searchParams.set(prop, data[prop]);
     });
 
-    console.log('posting access token to spotify');
+    console.log("posting access token to spotify");
     fetch(url, {
-      method: 'POST',
+      method: "POST",
       headers,
       body: searchParams,
     })
-      .then(res => res.json())
-      .then(credentials => {
+      .then((res) => res.json())
+      .then((credentials) => {
         req.credentials = credentials;
         next();
       })
@@ -40,22 +40,42 @@ const getSpotifyAccessToken = (req, res, next) => {
   }
 };
 
-const getAccessToken = (db, callback) => {
-  db.find({}, (err, docs) => {
-    if (err) {
-      console.error('Failed to retrieve documents');
-      throw Error('Failed to retrieve documents');
-    }
-    if (!docs) {
-      console.error('Documents are empty');
-      throw Error('Documents are empty');
-    }
+const storeAccessToken = (pool, token) => {
+  try {
+    pool.query(
+      "UPDATE credentials SET credential = $1 WHERE id = 1",
+      [token],
+      (error, results) => {
+        if (error) {
+          console.error(error);
+          throw error;
+        }
+        console.log(`storeAccessToken result: ${results}`);
+      }
+    );
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-    callback(docs[0].access_token);
-  });
+const getAccessToken = (pool, callback) => {
+  try {
+    pool.query("SELECT * FROM credentials WHERE id = 1", (error, results) => {
+      if (error) {
+        console.error(error);
+        throw error;
+      }
+      const credential = results.rows[0].credential;
+      const accessToken = JSON.parse(credential).access_token;
+      callback(accessToken);
+    });
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 module.exports = {
   getSpotifyAccessToken,
+  storeAccessToken,
   getAccessToken
 };
--- server/index.js
+++ server/index.js
@@ -10,21 +10,24 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const Datastore = require("nedb");
 const authorizeSpotify = require("./authorizeSpotify");
 const accessToken = require("./accessToken");
 const spotifyData = require("./spotifyData");
 
 const clientUrl = process.env.CLIENT_URL;
 const app = express();
-const db = new Datastore();
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const isDev = process.env.NODE_ENV !== "production";
 const PORT = process.env.PORT || 5000;
+
+const { Pool } = require("pg");
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: !isDev,
+});
 
 // Priority serve any static files.
 if (isDev) {
@@ -46,23 +49,31 @@ app.get("/node-env", (req, res) => {
             `);
 });
 
+app.get("/db-test", async (req, res) => {
+  try {
+    pool.query("SELECT * FROM credentials", (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.send("cred: " + results.rows[0].credential);
+    });
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
 app.get("/login", authorizeSpotify);
 
 app.get("/callback", accessToken.getSpotifyAccessToken, (req, res, next) => {
-  console.log("callback - credentials: " + JSON.stringify(req.credentials));
-  db.insert(req.credentials, (err) => {
-    if (err) {
-      next(err);
-    } else {
-      res.redirect(`${clientUrl}/?authorized=true`);
-    }
-  });
+//  console.log("callback - credentials: " + JSON.stringify(req.credentials));
+  accessToken.storeAccessToken(pool, req.credentials);
+  res.redirect(`${clientUrl}/?authorized=true`);
 });
 
 app.get("/history", (req, res) => {
-  accessToken.getAccessToken(db, (accessToken) => {
+  accessToken.getAccessToken(pool, (accessToken) => {
     const url = "https://api.spotify.com/v1/me/player/recently-played?limit=10";
-
     spotifyData
       .getSpotifyData(accessToken, url)
       .then((data) => {
@@ -77,9 +88,8 @@ app.get("/history", (req, res) => {
 });
 
 app.get("/playlists/:offset/:limit", (req, res) => {
-  accessToken.getAccessToken(db, (accessToken) => {
+  accessToken.getAccessToken(pool, (accessToken) => {
     const url = `https://api.spotify.com/v1/me/playlists?offset=${req.params.offset}&limit=${req.params.limit}`;
-
     spotifyData
       .getSpotifyData(accessToken, url)
       .then((data) => {
@@ -90,9 +100,8 @@ app.get("/playlists/:offset/:limit", (req, res) => {
 });
 
 app.get("/playlist/:id", (req, res) => {
-  accessToken.getAccessToken(db, (accessToken) => {
+  accessToken.getAccessToken(pool, (accessToken) => {
     const url = `https://api.spotify.com/v1/playlists/${req.params.id}`;
-
     spotifyData
       .getSpotifyData(accessToken, url)
       .then((data) => {
@@ -103,9 +112,8 @@ app.get("/playlist/:id", (req, res) => {
 });
 
 app.get("/playlist-tracks/:id/:offset/:limit", (req, res) => {
-  accessToken.getAccessToken(db, (accessToken) => {
+  accessToken.getAccessToken(pool, (accessToken) => {
     const url = `https://api.spotify.com/v1/playlists/${req.params.id}/tracks?offset=${req.params.offset}&limit=${req.params.limit}`;
-
     spotifyData
       .getSpotifyData(accessToken, url)
       .then((data) => {
@@ -116,10 +124,9 @@ app.get("/playlist-tracks/:id/:offset/:limit", (req, res) => {
 });
 
 app.post("/queue-track/:uri", (req, res) => {
-  accessToken.getAccessToken(db, (accessToken) => {
+  accessToken.getAccessToken(pool, (accessToken) => {
     console.log("queue track uri: " + req.params.uri);
     const url = `https://api.spotify.com/v1/me/player/queue?uri=${req.params.uri}`;
-
     spotifyData
       .postSpotifyData(accessToken, url)
       .then((data) => {
@@ -142,5 +149,5 @@ app.get("*", function (request, response) {
 });
 
 app.listen(PORT, function () {
-  console.log('Node server listening on port ${PORT}');
+  console.log(`Node server listening on port ${PORT}`);
 });
