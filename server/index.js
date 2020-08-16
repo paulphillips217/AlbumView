1c369b7661d5ec05175490d238e96955e83519a5
Paul Phillips <paulphillips217@gmail.com>
2020-08-16 17:47:02 -0400

    added worker test

--- server/index.js
+++ server/index.js
@@ -160,6 +160,47 @@ app.use(passport.session());
 
 /* this is the end of the OneDrive setup */
 
+/* start bull & redis queue setup */
+let Queue = require('bull');
+
+// Connect to a local redis instance locally, and the Heroku-provided URL in production
+let REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
+
+// Create / Connect to a named work queue
+let workQueue = new Queue('work', REDIS_URL);
+
+// Kick off a new job by adding it to the work queue
+app.get('/job', async (req, res) => {
+  console.log('kicking off a new job');
+  // This would be where you could pass arguments to the job
+  // Ex: workQueue.add({ url: 'https://www.heroku.com' })
+  // Docs: https://github.com/OptimalBits/bull/blob/develop/REFERENCE.md#queueadd
+  let job = await workQueue.add();
+  res.json({ id: job.id });
+});
+
+// Allows the client to query the state of a background job
+app.get('/job/:id', async (req, res) => {
+  let id = req.params.id;
+  let job = await workQueue.getJob(id);
+
+  if (job === null) {
+    res.status(404).end();
+  } else {
+    let state = await job.getState();
+    let progress = job._progress;
+    let reason = job.failedReason;
+    res.json({ id, state, progress, reason });
+  }
+});
+
+// You can listen to global events to get notified when jobs are processed
+workQueue.on('global:completed', (jobId, result) => {
+  console.log(`Job completed with result ${result}`);
+});
+
+/* end bull & redis queue setup */
+
 app.use('/spotify', spotifyRoutes);
 app.use('/one-drive', oneDriveRoutes);
 app.use('/last-fm', lastFmRoutes);
